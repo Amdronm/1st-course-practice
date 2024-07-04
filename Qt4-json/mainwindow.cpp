a424915510de0224fd36f0eee3579f02b0df713d
Andrey Svintilov <andrssvin@gmail.com>
2024-07-05 00:23:25 +0300

    final vers of Qt4

--- Qt4-json/mainwindow.cpp
+++ Qt4-json/mainwindow.cpp
@@ -6,9 +6,68 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    brush = new QBrush;
+    brush->setStyle(Qt::SolidPattern);
+    color = QColorConstants::Svg::violet;
+    pen = new QPen;
+    pen->setStyle(Qt::NoPen);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::paintEvent(QPaintEvent *)
+{
+    QPainter pain(this);
+    pain.begin(this);
+    brush->setColor(color);
+    pain.setBrush(*brush);
+    pain.setPen(*pen);
+    float x = 0, y = 0, w, h;
+    h = (v.size() == 0) ? 0 : (this->height() / v.size());
+    float ww = (this->width() - 350) / maxx;
+    for (size_t i = 0; i < v.size(); i++) {
+        w = ww * v[i];
+        pain.drawRect(QRectF(x, y + h / 4, w, h / 2));
+        y += h;
+    }
+    pain.end();
+}
+
+void MainWindow::on_buttonLoad_clicked()
+{
+    QString file_name = QFileDialog::getOpenFileName(0, "Open dialog", "", "*.json");
+    std::ifstream fin(file_name.toStdString());
+    if (!fin.is_open()) {
+        return;
+    }
+    jjj = nlohmann::json::parse(fin);
+    upd();
+    this->update();
+}
+
+void MainWindow::on_buttonAdd_clicked()
+{
+    jjj[ui->inKey->text().toStdString()] = ui->inValue->text().toInt();
+    v.push_back(ui->inValue->text().toInt());
+    upd();
+    this->update();
+}
+
+void MainWindow::on_buttonSave_clicked()
+{
+    QString file_name = QFileDialog::getOpenFileName(0, "Open dialog", "", "*.json");
+    std::ofstream fout(file_name.toStdString());
+    fout << jjj.dump(4);
+}
+
+void MainWindow::upd()
+{
+    v.clear();
+    for (const auto &i : jjj) {
+        v.push_back(i);
+    }
+    maxx = *std::max_element(v.begin(), v.end());
 }
--- Qt4-json/mainwindow.h
+++ Qt4-json/mainwindow.h
@@ -1,7 +1,15 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <QColor>
+#include <QColorDialog>
+#include <QFileDialog>
 #include <QMainWindow>
+#include <QPainter>
+
+#include <fstream>
+
+#include "json.hpp"
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -17,7 +25,25 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+protected:
+    void paintEvent(QPaintEvent *);
+
+private slots:
+    void on_buttonLoad_clicked();
+
+    void on_buttonAdd_clicked();
+
+    void on_buttonSave_clicked();
+
 private:
     Ui::MainWindow *ui;
+    QColor color;
+    QBrush *brush;
+    QPen *pen;
+    std::vector<int> v{};
+    nlohmann::json jjj;
+    int maxx;
+
+    void upd();
 };
 #endif // MAINWINDOW_H
